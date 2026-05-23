38e490a063e67bc6e0df1adedca2ed97fee32be4
ifoozann <mdwifauzan95@gmail.com>
2026-05-23 09:35:22 +0700

    fix build path and add postcss config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Pengaturan tambahan bisa ditulis di sini, 
-     tapi jika kosong, biarkan seperti di bawah */
+  output: 'standalone', 
 };
 
 export default nextConfig;
